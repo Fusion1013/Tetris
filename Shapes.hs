54c02e2f412ff071962653d06a96c20da9404c65
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2019-10-03 14:04:17 +0200

    Update Shapes.hs

--- Shapes.hs
+++ Shapes.hs
@@ -11,7 +11,7 @@ type Square = Maybe Colour
 data Colour = Black | Red | Green | Yellow | Blue | Purple | Cyan | Grey
               deriving (Eq,Bounded,Enum,Show)
   
--- | A geometric shape is represented as a list of lists of squares. Each square
+-- | A geometric rows is represented as a list of lists of squares. Each square
 -- can be empty or filled with a block of a specific colour.
   
 data Shape = S [Row] deriving (Eq)
@@ -74,7 +74,7 @@ allShapes = [S (makeSquares s) | s <- shapes]
 -- * Some simple functions
   
 -- ** A01
--- Creates an empty shape with the width and height specified
+-- Creates an empty rows with the width and height specified
 emptyShape :: (Int,Int) -> Shape
 emptyShape (width, height) = S (replicate height (createRow width))
   
@@ -83,25 +83,26 @@ createRow :: Int -> Row
 createRow width = replicate width Nothing
   
 -- ** A02
--- | The size (width and height) of a shape
+-- | The size (width and height) of a rows
 shapeSize :: Shape -> (Int,Int)
-shapeSize (S shape) = (length (head shape), length shape)
+shapeSize (S rows) = (length (head rows), length rows)
   
 -- ** A03
--- | Count how many non-empty squares a shape contains
---blockCount :: Shape -> Int
-blockCount (S shape) = length (concat shape) - emptySquares shape
-  where emptySquares shape = (length . filter (==Nothing)) (concat shape)
+-- | Count how many non-empty squares a rows contains
+blockCount :: Shape -> Int
+blockCount (S rows) = length (concat rows) - emptySquares rows
+  where emptySquares rows = (length . filter (==Nothing)) (concat rows)
 -- * The Shape invariant
   
 -- ** A04
 -- | Shape invariant (shapes have at least one row, at least one column,
 -- and are rectangular)
 prop_Shape :: Shape -> Bool
-prop_Shape (S x) | null x = False
-                  | length x == length (head x) && length x < 5 = True
-                  | otherwise = False
-  
+prop_Shape (S rows) | null rows = False
+                    | isSameLength rows = True
+                    | otherwise = False
+  where isSameLength rows = all (==length(head rows)) (map length rows)
+
   
 -- * Test data generators
 -- ** A05
@@ -123,52 +124,78 @@ instance Arbitrary Shape where
 -- * Transforming shapes
   
 -- ** A07
--- | Rotate a shape 90 degrees
+-- | Rotate a rows 90 degrees
 rotateShape :: Shape -> Shape
-rotateShape (S shape) = S (map reverse (transpose shape))
+rotateShape (S rows) = S (map reverse (transpose rows))
   
 -- ** A08
--- | shiftShape adds empty squares above and to the left of the shape
+-- | shiftShape adds empty squares above and to the left of the rows
 shiftShape :: (Int,Int) -> Shape -> Shape
-shiftShape (a, b) (S shape) = S (shiftRight a (shiftDown b shape))
+shiftShape (a, b) (S rows) = S (shiftRight a (shiftDown b rows))
   where
     shiftRight amount =
-      map (replicate amount Nothing ++ )
-    shiftDown amount shape = 
-      replicate amount (createRow (length (head shape))) ++ shape
+          map (replicate amount Nothing ++ )
+    shiftDown amount rows = 
+          replicate amount (createRow (length (head rows))) ++ rows
 
 -- ** A09
--- | padShape adds empty sqaure below and to the right of the shape
---padShape :: (Int,Int) -> Shape -> Shape
-padShape (a, b) (S shape) = S (shiftLeft a (shiftUp b shape))
+-- | padShape adds empty sqaure below and to the right of the rows
+padShape :: (Int,Int) -> Shape -> Shape
+padShape (a, b) (S rows) = S (shiftLeft a (shiftUp b rows))
   where
     shiftLeft amount =
       map (++ replicate amount Nothing)
-    shiftUp amount shape = 
-      shape ++ replicate amount (createRow (length (head shape)))
-  
+    shiftUp amount rows = 
+      rows ++ replicate amount (createRow (length (head rows)))
+
 -- ** A10
--- | pad a shape to a given size
+-- | pad a rows to a given size
 padShapeTo :: (Int,Int) -> Shape -> Shape
-padShapeTo (a, b) shape = padShape (a - first shape, b - second shape) shape
-  where first shape  = fst (shapeSize shape)
-        second shape = snd (shapeSize shape)
+padShapeTo (a, b) shape = padShape (a - width shape, b - height shape) shape
+  where width  shape = fst (shapeSize shape)
+        height shape = snd (shapeSize shape)
   
 -- * Comparing and combining shapes
   
 -- ** B01
   
 -- | Test if two shapes overlap
+
 overlaps :: Shape -> Shape -> Bool
-s1 `overlaps` s2 = error "A11 overlaps undefined"
-  
+(S r1) `overlaps` (S r2) = r1 `overlaps'` r2
+
+(x:xs) `overlaps'` (y:ys) | x `rowsOverlap` y = True
+                          | otherwise         = xs `overlaps'` ys
+_      `overlaps'` _                          = False
+
+rowsOverlap :: Row -> Row -> Bool
+(x:xs) `rowsOverlap` (y:ys) | isNothing x || isNothing y = xs `rowsOverlap` ys
+                            | otherwise                  = True
+_      `rowsOverlap` _                                   = False
+
 -- ** B02
 -- | zipShapeWith, like 'zipWith' for lists
 zipShapeWith :: (Square->Square->Square) -> Shape -> Shape -> Shape
-zipShapeWith = error "A12 zipShapeWith undefined"
-  
+zipShapeWith f (S s1) (S s2) = S (zipWith (zipWith f) s1 s2)
+
+blackClashes :: Shape -> Shape -> Shape
+blackClashes s1 s2 = zipShapeWith clash s1 s2  
+  where clash :: Square -> Square -> Square 
+        clash Nothing Nothing = Nothing
+        clash Nothing s       = s
+        clash s       Nothing = s
+        clash (Just c1) (Just c2) = Just Black
+
+
 -- ** B03
 -- | Combine two shapes. The two shapes should not overlap.
--- The resulting shape will be big enough to fit both shapes.
-combine :: Shape -> Shape -> Shape
-s1 `combine` s2 = error "A13 zipShapeWith undefined"
+-- The resulting rows will be big enough to fit both shapes.
+--combine :: Shape -> Shape -> Shape
+s1 `combine` s2 = blackClashes (pad s1 s2) (pad s2 s1)
+  where 
+    pad s1 s2 = padShapeTo (width s1 s2, height s1 s2) s1
+    height (S s1) (S s2) = max (length s1) (length s2)
+    width  (S s1) (S s2) = max (length (head s1)) (length (head s2))
+
+shape1 = allShapes !! 1
+shape2 = shiftShape (2,0) (allShapes !! 2)
