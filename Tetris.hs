24b1da11acf0ec9f7955d532e5f0ef7199bcf18f
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2019-10-08 11:08:19 +0200

    Update Tetris.hs

--- Tetris.hs
+++ Tetris.hs
@@ -92,6 +92,10 @@ startTetris rs = Tetris (startPosition,shape1) (emptyShape wellSize) supply
 -- and @'Just' (n,t)@, when the game continues in a new state @t@.
 stepTetris :: Action -> Tetris -> Maybe (Int,Tetris)
 stepTetris a t | a == Tick = tick t
+               | a == MoveDown = tick t
+               | a == MoveLeft = Just (0, movePiece (-1) t)
+               | a == MoveRight = Just (0, movePiece 1 t)
+               | a == Rotate = Just (0, rotatePiece t)
 stepTetris _ t = Just (0,t)
 
 -- Moves the tetris piece according to the vector v1
@@ -101,4 +105,33 @@ move v1 (Tetris (v2,p) w r) = Tetris (v1 `vAdd` v2, p) w r
 -- Gets called when a certain amount of time has passed
 -- Calls the function move and moves the piece one step down
 tick :: Tetris -> Maybe (Int, Tetris)
-tick t = Just (0, move (0, 1) t)
+tick t | collision (newState t) = Just (0, t)
+       | otherwise              = Just (0, newState t)
+  where newState t = move (0, 1) t
+
+  -- Checks if a piece has collided with something
+collision :: Tetris -> Bool
+collision (Tetris ((a, b), p) w r) | a < 0                          = True
+                                   | a + shapeWidth p > wellWidth   = True
+                                   | b + shapeHeight p > wellHeight = True
+                                   | p `overlaps` w                 = True
+                                   | otherwise                      = False
+  where shapeWidth (S rows) = length (head rows)
+        shapeHeight (S rows) = length rows
+
+movePiece :: Int -> Tetris -> Tetris
+movePiece n t | not (collision (newPos n t)) = newPos n t
+              | otherwise = t
+  where newPos n t = move (n, 0) t
+
+rotate :: Tetris -> Tetris
+rotate (Tetris (v, p) w r) = Tetris (v,(rotateShape p)) w r
+
+adjust :: Tetris -> Tetris
+adjust (Tetris ((a,b), p) w r) | a + shapeWidth p > wellWidth = (Tetris ((a - (a + shapeWidth p - wellWidth), b), p) w r)
+                               | otherwise = Tetris ((a,b), p) w r
+  where shapeWidth (S rows) = length (head rows)
+
+rotatePiece :: Tetris -> Tetris
+rotatePiece t | collision (adjust (rotate t)) = t
+              | otherwise                     = adjust (rotate t)
